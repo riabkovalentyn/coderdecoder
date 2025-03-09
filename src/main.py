a74a5cbfb0ec1ad12503ce01f88285b02f2f9280
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-03-09 17:35:23 +0100

    add main file

--- src/main.py
+++ src/main.py
@@ -2,4 +2,26 @@ import logging
 import json
 from sqlalchemy.orm import Session
 from test_db import engine, get_db
-from EncryptionSubscriber import EncryptionSubscriber
+from EncryptionSubscriber import EncryptionSub
+
+logging.basicConfig(
+    filename= 'app.log',
+    level= logging.INFO,
+    format = '%(message)s',
+)
+
+def log_json(level, message, **kwargs):
+    log_entry = {"level":level, "message": message, **kwargs}
+    logging.info(json.dumps(log_entry))
+
+def main():
+    log_json("info", "Application started")
+
+    db : Session = next(get_db())
+    log_json("info", "DB session established")
+
+    db.close()
+    log_json("info", "DB session closed")
+
+if __name__ == '__main__':
+    main()
